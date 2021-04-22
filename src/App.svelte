8541dc983413209e2790b024d0f74064db1e1b05
harryli0088 <harryli0088@gmail.com>
2021-04-22 11:50:21 -0400

    styled header

--- src/App.svelte
+++ src/App.svelte
@@ -7,11 +7,19 @@
 	$:globalValueString = globalValue.toString()
 </script>
 
-<main>
+<main style="--tablet-padding:calc(50vw - 300px + 1em);--desktop-padding:calc(50vw - 400px)">
 	<header>
-		<h1>Counting in Binary - Visualized</h1>
-		<div>
-			<a href="https://github.com/harryli0088/binary-visualized" target="_blank" rel="noopener noreferrer"><img src="img/github.svg" alt="github logo"/></a>
+		<div id="img-container">
+			<img src="img/nathan-lindahl-tX9A5DQHDC4-unsplash.jpg" alt="cat"/>
+			<div id="source">Photo by <a href="https://unsplash.com/@hip_dinosaur?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nathan Lindahl</a> on <a href="https://unsplash.com/s/photos/dashboard?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></div>
+		</div>
+
+		<div id="heading">
+			<h1>Binary Visualized</h1>
+			<p id="sub-heading">How car odometers can teach us to count in binary</p>
+			<div>
+				<a href="https://github.com/harryli0088/binary-visualized" target="_blank" rel="noopener noreferrer"><img src="img/github.svg" alt="github logo"/></a>
+			</div>
 		</div>
 	</header>
 
@@ -24,7 +32,7 @@
 			<li>This year is {now.getFullYear()}</li>
 		</ul>
 		<p><b>Base-ten</b> (aka <b>decimal</b>) simply means that we have ten symbols available to use for counting, ie 0, 1, 2, 3, 4, 5, 6, 7, 8, 9. When we get to the number <b>ten</b>, we don't have a dedicated symbol. Instead, we combine two symbols <b>1</b> and <b>0</b> to represent the number <b>10</b>.</p>
-		<p>Here is an odometer, which uses rotating dials to represent a multi-digit number. Try rotating the right-most dial and see how it behaves! What happens when you increment from nine to ten?</p>
+		<p>Odometers (like the one in your '95 Toyota Camery) use rotating dials to represent multi-digit numbers. Try rotating the right-most dial and see how it behaves! What happens when you increment from nine to ten?</p>
 		<Odometer base={10} initialValue={0} label="Value" places={2} showBaseTenValue={false}/>
 		<p>When we ran out of symbols in the <b>ones</b> place, we incremented the symbol in the <b>tens</b> place from 0 to 1. We also made a full revolution in the ones place from 9 back to 0.</p>
 		<Odometer base={10} initialValue={globalValue} label="Value" places={4} showBaseTenValue={false} bind:value={globalValue}/>
@@ -74,32 +82,84 @@
 	}
 	@media only screen and (min-width: 600px) {
 		header, section, footer {
-			padding-left: calc(50vw - 300px + 1em);
-			padding-right: calc(50vw - 300px + 1em);
+			padding-left: var(--tablet-padding);
+			padding-right: var(--tablet-padding);
 		}
 	}
 	@media only screen and (min-width: 1000px) {
 		header, section, footer {
-			padding-left: calc(50vw - 400px);
-			padding-right: calc(50vw - 400px);
+			padding-left: var(--desktop-padding);
+			padding-right: var(--desktop-padding);
 		}
 	}
 
 	header {
-		height: 70vh;
-		background-color: #ddd;
 		display: flex;
-		flex-direction: column;
+		flex-direction: column-reverse;
 		align-items: center;
 		justify-content: center;
+		overflow: hidden;
+		background-color: #17202A;
+		color: white;
+	}
+	@media only screen and (min-width: 600px) {
+		header {
+			height: 50vh;
+			flex-direction: row;
+		}
+	}
+	@media only screen and (min-width: 1000px) {
+		header {
+			height: 70vh;
+		}
 	}
 
-	header a img {
-		width: 2em;
-		height: 2em;
+
+	/*** start section for heading responsive styling ***/
+	#heading {
+		text-align: center;
+		padding-bottom: 1em;
+	}
+	@media only screen and (min-width: 600px) {
+		#img-container {
+			width: 50%;
+			padding-right: 1em;
+		}
+		#heading {
+			width: 50%;
+			padding: 0;
+			padding-left: 1em;
+			text-align: left;
+		}
+	}
+	/*** end section ***/
+
+	#img-container img {
+		width: 100%;
+		box-shadow: 0 0 5px rgba(0,0,0,0.1);
+	}
+
+	#source {
+		text-align: center;
+		font-size: 0.7em;
+		font-style: italic;
+		color: #bbb;
+	}
+
+	#heading h1 {
+		line-height: 1.25em;
+	}
+
+	#sub-heading {
+		font-style: italic;
+	}
+
+	#heading a img {
+		width: 1.75em;
+		height: 1.75em;
 		transition: 0.5s;
 	}
-	header a img:hover {
+	#heading a img:hover {
 		transform: scale(1.25);
 	}
 
